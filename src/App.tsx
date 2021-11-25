f01e5e95b8d48996af19198c9e548c04e28c8d0e
Matthew Hefferon <matthefferon@gmail.com>
2021-11-24 19:41:48 -0700

    Initial commit with area chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
+import Area from "./components/Area";
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Area Chart</h1>
+      <Area width={900} height={580} />
     </div>
   );
 }
