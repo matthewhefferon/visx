84a56dd5bb923aecfa3e306bb51269c092663636
Matthew Hefferon <matthefferon@gmail.com>
2021-11-26 16:10:00 -0700

    Updating KPI's

--- src/components/KPI.tsx
+++ src/components/KPI.tsx
@@ -1,23 +1,54 @@
 import React from "react";
-import { render } from "react-dom";
-import { Text } from "@visx/text";
+// import { Text } from "@visx/text";
 
 const KPI = () => (
   <div className="container mx-auto flex justify-evenly pt-10">
-    <div className="shadow-md rounded-md bg-white">
+    <div className="shadow-2xl rounded-md bg-primary">
       <div className="p-5">
-        <h5 className="text-xl font-semibold mb-2">
+        <div className="text-xl font-semibold mb-2 text-white">
           Daily Active Users (DAU)
           {/* <svg>
             <Text verticalAnchor="middle">Daily Active Users (DAU)</Text>
           </svg> */}
-        </h5>
-        <h1 className="text-5xl font-semibold mb-2 flex flex-center justify-center p-5">
+        </div>
+        <div className="text-5xl font-semibold mb-2 flex flex-center justify-center p-5 text-white">
           200K
           {/* <svg>
-            <Text verticalAnchor="middle" fontSize="100px">200K</Text>
+            <Text verticalAnchor="middle" style={{ fontWeight: 900 }} fontSize="100px">200K</Text>
           </svg> */}
-        </h1>
+        </div>
+      </div>
+    </div>
+    <div className="shadow-2xl rounded-md bg-primary">
+      <div className="p-5">
+        <div className="text-xl font-semibold mb-2 text-white">
+          Daily Active Users (DAU)
+          {/* <svg>
+            <Text verticalAnchor="middle">Daily Active Users (DAU)</Text>
+          </svg> */}
+        </div>
+        <div className="text-5xl font-semibold mb-2 flex flex-center justify-center p-5 text-white">
+          200K
+          {/* <svg>
+            <Text verticalAnchor="middle" style={{ fontWeight: 900 }} fontSize="100px">200K</Text>
+          </svg> */}
+        </div>
+      </div>
+    </div>
+    <div className="shadow-2xl rounded-md bg-primary">
+      <div className="p-5">
+        <div className="text-xl font-semibold mb-2 text-white">
+          Daily Active Users (DAU)
+          {/* <svg>
+            <Text verticalAnchor="middle">Daily Active Users (DAU)</Text>
+          </svg> */}
+        </div>
+        <div className="text-5xl font-semibold mb-2 flex flex-center justify-center p-5 text-white">
+          200K
+          {/* <svg>
+            <Text verticalAnchor="middle" style={{ fontWeight: 900 }} fontSize="100px">200K</Text>
+          </svg> */}
+        </div>
       </div>
     </div>
   </div>
