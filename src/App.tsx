158e51ab2a7297320df9af1312b74824d5e02add
Matthew Hefferon <matthefferon@gmail.com>
2021-11-27 18:59:59 -0700

    Added bar chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Area from "./components/Area";
 import "./App.css";
 import KPI from "./components/KPI";
+import Bars from "./components/Bars";
 
 function App() {
   return (
-    <div className="container mx-auto flex-col flex self-center p-10">
+    <div className="container mx-auto px-4 flex-col flex self-center p-10">
       <KPI />
       <div className="p-10 self-center ">
         <Area width={900} height={580} />
+      </div>
+      <div className="p-10 self-center ">
+        <Bars width={900} height={580} />
       </div>
     </div>
   );
